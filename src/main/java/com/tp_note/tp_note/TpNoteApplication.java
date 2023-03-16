d3a925a7007719c5471d73e2147cafebc6abe441
faniryxx <faniry597@gmail.com>
2023-03-16 09:57:45 +0100

    Initial commit

---
+++ src/main/java/com/tp_note/tp_note/TpNoteApplication.java
@@ -0,0 +1,13 @@
+package com.tp_note.tp_note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tp_note/tp_note/TpNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp_note.tp_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
