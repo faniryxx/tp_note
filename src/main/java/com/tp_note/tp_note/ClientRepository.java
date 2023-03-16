3ed193857183e5d1b0428306bb272f6010bbf0ec
faniryxx <faniry597@gmail.com>
2023-03-16 11:38:14 +0100

    Renamede ClientRepository.java

--- src/main/java/com/tp_note/tp_note/ClientRepository.java
+++ src/main/java/com/tp_note/tp_note/ClientRepository.java
@@ -1,13 +1,11 @@
 package com.tp_note.tp_note;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class SpringJdbcClientDaoImpl{
+public class ClientRepository {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
