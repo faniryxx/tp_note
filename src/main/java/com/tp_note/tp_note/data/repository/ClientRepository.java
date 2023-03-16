3103272be41dc6e33d19f591a57a4ffdbc4ffc8e
faniryxx <faniry597@gmail.com>
2023-03-16 14:27:42 +0100

    Update ClientRepository

--- src/main/java/com/tp_note/tp_note/data/repository/ClientRepository.java
+++ src/main/java/com/tp_note/tp_note/data/repository/ClientRepository.java
@@ -1,14 +1,12 @@
 package com.tp_note.tp_note.data.repository;
 
 import com.tp_note.tp_note.ClientEntity;
+import com.tp_note.tp_note.ContratEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import com.tp_note.tp_note.ClientEntity;
-
 import java.util.List;
-
 
 @Repository
 public class ClientRepository {
@@ -16,14 +14,18 @@ public class ClientRepository {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
-
-	public ClientEntity findById(Long clientId){
-		String sql = "SELECT * FROM client WHERE id_client="+clientId.toString()+";";
-		return this.jdbcTemplate.queryForObject(sql, ClientEntity.class);
-	}
-
 	public List listerClients(){
 		String sql = "SELECT * FROM client;";
+		return this.jdbcTemplate.queryForList(sql);
+	}
+
+	public List getContrats(Long clientId) {
+		String sql = "SELECT * FROM contrat WHERE id_client="+clientId.toString()+";";
+		return this.jdbcTemplate.queryForList(sql);
+	}
+
+	public List findContratByIdAndContratId(Long clientId, Long contratId) {
+		String sql = "SELECT * FROM contrat WHERE id_client="+clientId.toString()+" AND id_contrat="+contratId.toString()+";";
 		return this.jdbcTemplate.queryForList(sql);
 	}
 }
