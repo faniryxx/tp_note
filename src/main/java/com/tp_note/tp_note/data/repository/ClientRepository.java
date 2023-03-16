202ec96b306b109a1efcaf780d43940a8a35d291
faniryxx <faniry597@gmail.com>
2023-03-16 17:01:21 +0100

    Removed unused imports

--- src/main/java/com/tp_note/tp_note/data/repository/ClientRepository.java
+++ src/main/java/com/tp_note/tp_note/data/repository/ClientRepository.java
@@ -1,14 +1,8 @@
 package com.tp_note.tp_note.data.repository;
 
 import com.tp_note.tp_note.ClientEntity;
-import com.tp_note.tp_note.ContratEntity;
-import com.tp_note.tp_note.service.ClientService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ClientRepository extends JpaRepository<ClientEntity, Integer> {
