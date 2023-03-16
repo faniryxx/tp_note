ff599238a97017f87eaf881cddc653ae6bd96499
faniryxx <faniry597@gmail.com>
2023-03-16 11:13:22 +0100

    Update controllerClient.java

--- src/main/java/com/tp_note/tp_note/controller/controllerClient.java
+++ src/main/java/com/tp_note/tp_note/controller/controllerClient.java
@@ -1,3 +1,11 @@
+package com.tp_note.tp_note.controller;
+
+import com.tp_note.tp_note.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/clients")
 
