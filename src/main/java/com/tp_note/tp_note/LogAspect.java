d83856688a97a6037e1795ea7869d0ef61d1c8c6
Basile8 <104374345+Basile8@users.noreply.github.com>
2023-03-17 13:41:02 +0100

    Update_Loggers01_BGA

--- src/main/java/com/tp_note/tp_note/LogAspect.java
+++ src/main/java/com/tp_note/tp_note/LogAspect.java
@@ -1,32 +1,25 @@
 package com.tp_note.tp_note;
 
 import org.apache.logging.log4j.*;
-import org.aspectj.lang.*;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+
+
 @Component(value="logaspect")
 @Aspect
 public class LogAspect {
   private final static Logger LOG = LogManager.getLogger();
 
-  @Before("execution( * com.tp_note.tp_note.data.repository.ClientRepository.findById())")
-  public void logBeforeClientEntity(JoinPoint jp) {
-    LogAspect.LOG.info("Appel de la fonction findById de l'entité Client");
-  }
-
-  @After("execution( * com.tp_note.tp_note.data.repository.ClientRepository.findById())")
-  public void logAfterClientEntity(JoinPoint jp) {
-    LogAspect.LOG.info("Appel de la fonction findById de l'entité Client");
-  }
   
-  @Before("execution( * com.tp_note.tp_note.data.repository.ConseillerRepository.findById())")
-  public void logBeforeConseillerEntity(JoinPoint jp) {
-    LogAspect.LOG.info("Appel de la fonction findById de l'entité Conseiller");
+  @After("execution( * com.tp_note.tp_note.service.impl.ClientServiceImpl.*find*(..))")
+  public void logBeforeClientService() {
+    LogAspect.LOG.info("Appel Client Service");
   }
 
-  @After("execution( * com.tp_note.tp_note.data.repository.ConseillerRepository.findById())")
-  public void logAfterConseillerEntity(JoinPoint jp) {
-    LogAspect.LOG.info("Appel de la fonction findById de l'entité Conseiller");
+  
+  @After("execution( * com.tp_note.tp_note.service.impl.ConseillerServiceImpl.*find*(..))")
+  public void logAfterClientService() {
+    LogAspect.LOG.info("Appel Conseiller Service");
   }
 }
 	
