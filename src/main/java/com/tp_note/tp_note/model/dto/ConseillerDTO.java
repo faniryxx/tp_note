671aabdfeb1b02eca81cd6f90ab3603473fa521e
val-moizan <51909360+val-moizan@users.noreply.github.com>
2023-03-17 08:08:11 +0100

    ajout clients du conseiller dto

---
+++ src/main/java/com/tp_note/tp_note/model/dto/ConseillerDTO.java
@@ -0,0 +1,71 @@
+package com.tp_note.tp_note.model.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.tp_note.tp_note.ConseillerEntity;
+
+
+public class ConseillerDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	private Integer idConseiller;
+
+	private String nom;
+
+	private String prenom;
+	
+	private List<ClientDTO> clients;
+
+	public ConseillerDTO() {
+		super();
+	}
+	
+	public ConseillerDTO(final ConseillerEntity entity) {
+		this.idConseiller = entity.getIdConseiller();
+		this.nom = entity.getNom();
+		this.prenom = entity.getPrenom();
+	}
+
+	public ConseillerEntity toEntity(){
+		ConseillerEntity entity = new ConseillerEntity();
+		entity.setIdConseiller(this.idConseiller);
+		entity.setNom(this.nom);
+		entity.setPrenom(this.prenom);
+		return entity;
+	}
+
+	public Integer getIdConseiller() {
+		return idConseiller;
+	}
+
+	public void setIdConseiller(Integer idConseiller) {
+		this.idConseiller = idConseiller;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public List<ClientDTO> getClients() {
+		return clients;
+	}
+
+	public void setClients(List<ClientDTO> clients) {
+		this.clients = clients;
+	}
+	
+	
+}
